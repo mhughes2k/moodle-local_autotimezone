b2d656a7476b218bb4d50e24479199fd4f7078a2
Michael hughes <michaelhughes@strath.ac.uk>
2025-01-28 12:13:44 +0000

    CONTRIB-9795 local_autotimezone: Fixing review issues

--- classes/external/update_timezone.php
+++ classes/external/update_timezone.php
@@ -1,9 +1,31 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
 namespace local_autotimezone\external;
 use core_external\external_function_parameters;
 use core_external\external_value;
 use core_external\external_single_structure;
 require_once($CFG->dirroot . '/user/lib.php');
+
+/**
+ * @package     local_autotimezone
+ * @copyright   2025 Univesity of Strathclyde <learning-technologies@strath.ac.uk>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 class update_timezone extends \core_external\external_api {
 
     public static function execute_parameters() {
